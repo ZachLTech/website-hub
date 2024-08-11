19e1c821cc5fcd7db5b4e7cf576c043d156094c9
ZachLTech <zlopezzl2021@gmail.com>
2024-08-11 18:09:26 -0400

    done - no 404 href

--- site/components/door.vue
+++ site/components/door.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :style="{ background: background }" :href="location" class="w-[300px] mb-12 p-2 flex flex-col items-center gap-8 shadow-inner rounded-lg">
+    <a :style="{ background: background }" :href="location" class="w-[300px] mb-12 p-2 flex flex-col items-center gap-8 rounded-lg">
         <div class="flex w-full justify-between">
             <p class="text-xl">{{ icon }} {{ title }}</p>
             <div :style="{ backgroundColor: tagColor }" class="px-1 text-lg rounded-lg"> {{ tag }} </div>
--- site/pages/index.vue
+++ site/pages/index.vue
@@ -15,78 +15,47 @@
                     </p>
                 </div>
             </div>
-            <div class="w-full flex flex-col flex-wrap max-h-[1500px]">
+            <div class="w-full flex flex-wrap justify-between max-h-[1800px]">
+                <door :icon="'👤'" :title="'Socials'" :location="'https://zachl.tech/'" :description="'My socials and contact info if you want to message me or reach out'" :tag="'Info'" :tagColor="'#1c567a'" />
                 <door :icon="'🗃️'" :title="'Portfolio'" :location="'#'" :description="'My portfolio site is currently non-existent and I will soon make it'" :tag="'Branding'" :tagColor="'#43b046'" /> 
                 <door :icon="'🍱'" :title="'Bentofolio'" :location="'https://bentofolio.zachl.tech'" :description="'My bento grid style portfolio - Not my official portfolio site though'" :tag="'Branding'" :tagColor="'#43b046'" /> 
                 <door :icon="'💼'" :title="'Résumé (Site)'" :location="'https://resume.zachl.tech'" :description="'View my résumé in a nice, less professional, website format'" :tag="'Branding'" :tagColor="'#43b046'" /> 
-                <door :icon="'📁'" :title="'Résumé (PDF)'" :location="'https://resume-pdf.zachl.tech'" :description="'View my résumé as a professional PDF'" :tag="'Branding'" :tagColor="'#43b046'" /> 
+                <door :icon="'📁'" :title="'Résumé (PDF)'" :location="'https://resume-pdf.zachl.tech'" :description="'View my résumé as a professional PDF with some redacted info (May be outdated)'" :tag="'Branding'" :tagColor="'#43b046'" /> 
                 <door :icon="'🔑'" :title="'SSHfolio (SSH)'" :location="'https://github.com/ZachLTech/sshfolio'" :description="'Minimally showing off all your unique talents through a publicly SSHable TUI interface written in Go'" :tag="'Branding'" :tagColor="'#43b046'" /> 
                 <door :icon="'📚'" :title="'Blog'" :location="'https://blog.zachl.tech'" :description="'Read some of my stories, tutorials, and see my thought process while making projects here'" :tag="'Branding'" :tagColor="'#43b046'" /> 
                 <door :icon="'🏎️'" :title="'AEV-Software'" :location="'https://github.com/YamanDevelopment/AEV-Software'" :description="'The Alset Solar Cybersedan Software is a comprehensive full-stack ecosystem developed for the FAUHS AEV solar car'" :tag="'Project'" :tagColor="'#947100'" /> 
-                <door :icon="'💻'" :title="'Devfolify'" :location="'https://github.com/YamanDevelopment/Devfolify'" :description="'Giving you real world problems and a chance to solve it using code in your unique way. We\'re here to help you develop unique projects and real-world problem-solving skills.'" :tag="'WIP Project'" :tagColor="'#947100'" /> 
+                <door :icon="'💻'" :title="'Devfolify'" :location="'https://github.com/YamanDevelopment/Devfolify'" :description="'Giving you real world problems and a chance to solve it using code in your unique way.'" :tag="'WIP Project'" :tagColor="'#947100'" /> 
                 <door :icon="'📅'" :title="'Schedulix'" :location="'https://github.com/YamanDevelopment/Schedulix'" :description="'A program that helps university students develop their course schedules for their upcoming semester.'" :tag="'WIP Project'" :tagColor="'#947100'" /> 
-                <door :icon="'☕'" :title="'Network Chuck'" :location="'https://www.youtube.com/@NetworkChuck'" :description="'His tutorials and educational videos are so great and he was the reason I tried Linux for the first time'" :tag="'Youtuber'" :tagColor="'#FF0000'" /> 
-                <door :icon="'️‍🔥'" :title="'Fireship'" :location="'https://www.youtube.com/@Fireship'" :description="'The entertainment & education combo in Fireships videos are simply unmatched and I tend to get a lot of my tech news from him now'" :tag="'Youtuber'" :tagColor="'#FF0000'" /> 
-                <door :icon="'🍳'" :title="'Frying Pan'" :location="'https://www.youtube.com/@FryingPan'" :description="'Frying Pans skits and journey helped inspire me to continue in CS alongside attempting a startup'" :tag="'Youtuber'" :tagColor="'#FF0000'" /> 
-                <door :icon="'🦉'" :title="'Raid Owl'" :location="'https://www.youtube.com/@RaidOwl'" :description="'Without RaidOwl, I probably would not have as secure of a homelab setup as I do now nor would I be using raid redundancy'" :tag="'Youtuber'" :tagColor="'#FF0000'" /> 
                 <door :icon="'👩‍💻'" :title="'Matrix'" :location="'https://matrix.yamanon.top/'" :description="'Matrix protocol for use with my friends since other forms of communication are pretty unprivate'" :tag="'Homelab'" :tagColor="'#2922ab'" /> 
                 <door :icon="'🎥'" :title="'Movie-Web'" :location="'https://movie-web.zachl.tech'" :description="'Movie-Web is a very convenient media streaming tool which I use all the time when watching The Office'" :tag="'Homelab'" :tagColor="'#2922ab'" /> 
                 <door :icon="'📄'" :title="'Fileshare'" :location="'https://share.lopezhome.tech'" :description="'The convenience of having my own filesharing service is so nice'" :tag="'Homelab'" :tagColor="'#2922ab'" /> 
                 <door :icon="'🔍'" :title="'SearXNG'" :location="'https://search.lopezhome.tech'" :description="'Why use Google or DuckDuckGo when you can use a combination of them all, AND keep your data private!!'" :tag="'Homelab'" :tagColor="'#2922ab'" /> 
                 <door :icon="'🤠'" :title="'Thandi'" :location="'https://str1ke.codes'" :description="'A self-taught full-stack developer who I partner up with for various projects and Hackathons'" :tag="'Friend'" :tagColor="'#276266'" /> 
-                <door :icon="'😱'" :title="'Amarnath'" :location="'https://github.com/jeebuscrossaint'" :description="'Another close friend of mine who took up programming as he\'s, as he says, always found the science behind computers and computational intelligence very interesting'" :tag="'Friend'" :tagColor="'#276266'" /> 
+                <door :icon="'😱'" :title="'Amarnath'" :location="'https://github.com/jeebuscrossaint'" :description="'Another close friend of mine who took up programming due to his interest in the science behind computers'" :tag="'Friend'" :tagColor="'#276266'" /> 
                 <door :icon="'🤯'" :title="'Jossaya'" :location="'https://jcamille.tech/'" :description="'Jossaya is a great guy who\'s typically ready to jump head first into any coding project'" :tag="'Friend'" :tagColor="'#276266'" /> 
                 <door :icon="'😂'" :title="'Makai'" :location="'https://makaip.com'" :description="'A very enthusiastic friend who, as he says, enjoys coding, making stuff in Blender, playing the piano, and photography'" :tag="'Friend'" :tagColor="'#276266'" /> 
                 <door :icon="'😎'" :title="'Alex'" :location="'https://www.alexcastronovo.com/'" :description="'A research partner I have yet to properly befriend who has experience in many CS related fields'" :tag="'Friend'" :tagColor="'#276266'" /> 
+                <door :icon="'☕'" :title="'Network Chuck'" :location="'https://www.youtube.com/@NetworkChuck'" :description="'His tutorials and educational videos are so great and he was the reason I tried Linux for the first time'" :tag="'Creator'" :tagColor="'#FF0000'" /> 
+                <door :icon="'️‍🔥'" :title="'Fireship'" :location="'https://www.youtube.com/@Fireship'" :description="'The entertainment & education combo in Fireships videos are simply unmatched and I tend to get a lot of my tech news from him now'" :tag="'Creator'" :tagColor="'#FF0000'" /> 
+                <door :icon="'🍳'" :title="'Frying Pan'" :location="'https://www.youtube.com/@FryingPan'" :description="'Frying Pans skits and journey helped inspire me to continue in CS alongside attempting a startup'" :tag="'Creator'" :tagColor="'#FF0000'" /> 
+                <door :icon="'🦉'" :title="'Raid Owl'" :location="'https://www.youtube.com/@RaidOwl'" :description="'Without RaidOwl, I probably would not have as secure of a homelab setup as I do now nor would I be using raid redundancy'" :tag="'Creator'" :tagColor="'#FF0000'" /> 
             </div>
             <!-- <div class="flex justify-between items-center w-full">
                 <div class="w-[45%] h-[1px] bg-white"></div>
                 <div class="w-6 h-6 bg-white rounded-full"></div>
                 <div class="w-[45%] h-[1px] bg-white"></div>
             </div> -->
-            <section class="bg-black h-[35vh] w-screen sm:h-[50vh]"></section>
-            <!-- <section class="fixed -z-20 h-[35vh] sm:h-[50vh] bottom-0 flex flex-col justify-center items-center gap-5 sm:gap-12 p-5 sm:p-36">
-                
-                <div class="w-full sm:h-full flex items-center sm:gap-[250px]">
-                
-                <div class="flex flex-col justify-center items-center sm:justify-start sm:items-start self-start gap-3">
-                    <p class="text-xl text-gray-200 text-center sm:text-left">We make AI-generated text undetectable.</p>
-                </div>
-                
-                <div class="hidden sm:flex gap-24">
-                    <ul>
-                    <li class="text-3xl text-white font-semibold mb-4">Try Now</li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Sign up</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Login</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Demo</NuxtLink></li>
-                    </ul>
-                    <ul>
-                    <li class="text-3xl text-white font-semibold mb-4">Features</li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Undetector</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Generator</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Detector</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Camouflage</NuxtLink></li>
-                    </ul>
-                    <ul>
-                    <li class="text-3xl text-white font-semibold mb-4">Support</li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Contact</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Pricing</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Documentation</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Guides</NuxtLink></li>
-                    </ul>
-                    <ul>
-                    <li class="text-3xl text-white font-semibold mb-4">Company</li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">About</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Press</NuxtLink></li>
-                    <li class="text-lg text-gray-200 mb-2 cursor-pointer"><NuxtLink to="">Partners</NuxtLink></li>
-                    </ul>
-                </div>
-                </div>
-                
-                <div class="w-full h-[1px] bg-gray-500">ㅤ</div>
-                
-                <h1 class="text-lg text-gray-500 self-center sm:self-start text-center sm:text-left">© 2024 Chameleon, LLC. All rights reserved.</h1>
-            </section> -->
+            <section class="p-4 bg-black h-[50vh] w-screen flex items-end poppins">
+                <a href="">
+                    <pre class="font-mono text-md leading-none select-none">
+_|  _|      _|    _|  _|   
+_|  _|    _|  _|  _|  _|   
+_|_ _|_|  _|  _|  _|_ _|_| 
+    _|    _|  _|      _|   
+    _|      _|        _|   
+                    </pre>
+                </a>
+            </section>
         </div>
     </div>
 </template>
