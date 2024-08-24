f8b9454e78f8ed0e3b77979eff906eac4ffa4625
ZachLTech <zlopezzl2021@gmail.com>
2024-08-24 02:54:48 -0400

    finished module sizing & layout

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,76 +1,101 @@
 <template>
-    <div class="w-screen h-screen bg-black flex flex-col justify-center items-center">
-        <swiper
-            class="w-[70vw] h-[45vh] bg-slate-900"
-            :modules="modules"
-            :slides-per-view="1"
-            :space-between="150"
-            navigation
-            :pagination="{ clickable: true }"
-            :scrollbar="{ draggable: true }"
-            @swiper="onSwiper"
-            @slideChange="onSlideChange"
-        >
-            <swiper-slide>
-                <div class="h-full flex flex-col justify-between p-16">
-                    <div class="flex flex-wrap justify-between">
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                    </div>
-                    <div class="flex flex-wrap justify-between">
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                        <div class="flex flex-col justify-center items-center gap-2">
-                            <div class="w-40 h-40 bg-black rounded-[25%] shadow-lg"></div>
-                            <p class="text-xl">App 1</p>
-                        </div>
-                    </div>
+    <div class="w-screen h-screen p-4 bg-white flex flex-col justify-between items-center">
+        <div class="flex flex-col justify-center items-center m-6">
+            <h1 class="text-5xl font-bold">Welcome to my link lobby.</h1>
+        </div>
+        <div class="flex flex-col justify-center items-center">
+            <div class="w-[65vw] flex justify-between items-center">
+                <div class="flex flex-col justify-center items-center gap-2">
+                    <div class="w-[20.5vw] h-[20vh] rounded-[2rem] bg-slate-400 "></div>
+                    <p class="text-xl">Tile 1</p>
                 </div>
-            </swiper-slide>
-            <swiper-slide>Slide 2</swiper-slide>
-            <swiper-slide>Slide 3</swiper-slide>
-            ...
-        </swiper>
+                <div class="flex flex-col justify-center items-center gap-2">
+                    <div class="w-[20.5vw] h-[20vh] rounded-[2rem] bg-slate-400 "></div>
+                    <p class="text-xl">Tile 1</p>
+                </div>
+                <div class="flex flex-col justify-center items-center gap-2">
+                    <div class="w-[20.5vw] h-[20vh] rounded-[2rem] bg-slate-400 "></div>
+                    <p class="text-xl">Tile 1</p>
+                </div>
+            </div>
+            <swiper
+                class="w-[70vw] h-[45vh]"
+                :modules="modules"
+                :slides-per-view="1"
+                :space-between="150"
+                navigation
+                :pagination="{ clickable: true }"
+                @swiper="onSwiper"
+                @slideChange="onSlideChange"
+            >
+                <swiper-slide>
+                    <div class="h-full flex flex-col justify-between px-16 py-8">
+                        <div class="flex flex-wrap justify-between">
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                        </div>
+                        <div class="flex flex-wrap justify-between">
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                            <div class="flex flex-col justify-center items-center gap-2">
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <p class="text-xl">App 1</p>
+                            </div>
+                        </div>
+                    </div>
+                </swiper-slide>
+                <swiper-slide>Slide 2</swiper-slide>
+                <swiper-slide>Slide 3</swiper-slide>
+                ...
+            </swiper>
+        </div>
+        <div class="w-[30vw] h-[12vh] px-3 bg-slate-300 rounded-[2rem] flex justify-around items-center">
+            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
+            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
+            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
+            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
+            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
+        </div>
     </div>
 </template>
 <script>
