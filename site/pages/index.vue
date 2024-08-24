2ecea71ea25b0d98f70b406735e081426ed1ba5b
ZachLTech <zlopezzl2021@gmail.com>
2024-08-24 11:54:55 -0400

    modal experimenting

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="w-screen h-screen p-4 bg-white flex flex-col justify-between items-center">
+        <div v-if="showModal" @click="closeModal" :class="{'fade-in': showModal, 'fade-out': !showModal, 'flex': showModal, 'hidden': !showModal}" class="fixed inset-0 bg-black bg-opacity-50 justify-center items-center z-50 transition-opacity duration-500">
+            <div @click.stop :class="{'scale-in': showModal, 'scale-out': !showModal}" class="w-96 bg-white rounded-lg shadow-lg p-6 transition-transform duration-500">
+                <h2 class="text-2xl mb-4">{{ selectedApp }} Details</h2>
+                <p v-if="selectedApp === 'App 1'">This is the content for App 1.</p>
+                <p v-if="selectedApp === 'App 2'">This is the content for App 2.</p>
+                <p v-if="selectedApp === 'App 3'">This is the content for App 3.</p>
+                <!-- Add more content based on the selectedApp value -->
+            </div>
+        </div>
         <div class="flex flex-col justify-center items-center m-6">
             <h1 class="text-5xl font-bold">Welcome to my link lobby.</h1>
         </div>
@@ -32,7 +41,7 @@
                     <div class="h-full flex flex-col justify-between px-16 py-8">
                         <div class="flex flex-wrap justify-between">
                             <div class="flex flex-col justify-center items-center gap-2">
-                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg"></div>
+                                <div class="w-32 h-32 bg-black rounded-[25%] shadow-lg" @click="openModal('App 1')"></div>
                                 <p class="text-xl">App 1</p>
                             </div>
                             <div class="flex flex-col justify-center items-center gap-2">
@@ -90,11 +99,31 @@
             </swiper>
         </div>
         <div class="w-[30vw] h-[12vh] px-3 bg-slate-300 rounded-[2rem] flex justify-around items-center">
-            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
-            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
-            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
-            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
-            <div class="w-24 h-24 bg-black rounded-[25%] shadow-lg"></div>
+            <div class="relative group w-24 h-24 bg-black rounded-[25%] shadow-lg">
+                <span class="tooltip-text absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 w-max px-2 py-1 bg-gray-700 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                    App 1
+                </span>
+            </div>
+            <div class="relative group w-24 h-24 bg-black rounded-[25%] shadow-lg">
+                <span class="tooltip-text absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 w-max px-2 py-1 bg-gray-700 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                    App 1
+                </span>
+            </div>
+            <div class="relative group w-24 h-24 bg-black rounded-[25%] shadow-lg">
+                <span class="tooltip-text absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 w-max px-2 py-1 bg-gray-700 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                    App 1
+                </span>
+            </div>
+            <div class="relative group w-24 h-24 bg-black rounded-[25%] shadow-lg">
+                <span class="tooltip-text absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 w-max px-2 py-1 bg-gray-700 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                    App 1
+                </span>
+            </div>
+            <div class="relative group w-24 h-24 bg-black rounded-[25%] shadow-lg">
+                <span class="tooltip-text absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 w-max px-2 py-1 bg-gray-700 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                    App 1
+                </span>
+            </div>
         </div>
     </div>
 </template>
@@ -130,5 +159,21 @@
         modules: [Navigation, Pagination, Scrollbar, A11y],
       };
     },
+    data() {
+        return {
+            showModal: false,
+            selectedApp: null
+        };
+    },
+    methods: {
+        openModal(app) {
+            this.selectedApp = app;
+            this.showModal = true;
+        },
+        closeModal() {
+            this.showModal = false;
+            this.selectedApp = null;
+        }
+    }
   };
 </script>
